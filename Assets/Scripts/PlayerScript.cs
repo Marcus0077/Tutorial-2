baff7e1bfbf7734b05c33530683d445d9f78c1c4
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 22:21:42 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,56 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
-    public Text score;
-
-    private int scoreValue = 0;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI livesText;
+    private int count;
+    private int lives;
+    public GameObject Player;
+    public AudioSource audio1;
+    public AudioSource audio2;
 
     // Start is called before the first frame update
     void Start()
     {
+        audio1.Play();
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        count = 0;
+        lives = 3;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        float verMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
-        }  
-    }
+            count = count + 1;
+            SetCountText();
+        }
 
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;
+            SetCountText();
+        }
+    }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 2), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors. You can also create a public variable for it and then edit it in the inspector.
+                rd2d.AddForce(new Vector2(0.0f, 3.0f), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+
+        if (count >= 4)
+        {
+            Destroy(this);
+            audio1.Stop();
+            audio2.Play();
+            winText.text = "You Win!\nGame Created By Marcus Werner";
+        }
+
+        if (lives <= 0)
+        {
+            Destroy(this);
+            winText.text = "You Lose!\nGame Created By Marcus Werner";
         }
     }
 }
